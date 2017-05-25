babd642bcb87c66e94d5e4296f8951a052b09741
gabriel marq <gabriel@go2web.com.br>
2017-05-25 19:10:55 -0300

    Classes pojo public

--- app/src/main/java/br/com/marks/brinquedoteca/a/model/Crianca.java
+++ app/src/main/java/br/com/marks/brinquedoteca/a/model/Crianca.java
@@ -5,27 +5,13 @@ package br.com.marks.brinquedoteca.a.model;
  */
 
 public class Crianca {
-    private String nome;
-    private Integer idade;
 
-    public Crianca(String nome, Integer idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
+    //Basic
+    public String nome; //Nome Completo
+    public Integer idade; //Numericos simples: 5/6/7/8 anos
+    public Integer identidade;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
+    //IMGUR
+    public String urlImagem;
+    public String urlHash;
 }
---
+++ app/src/main/java/br/com/marks/brinquedoteca/a/model/Usuario.java
@@ -0,0 +1,28 @@
+package br.com.marks.brinquedoteca.a.model;
+
+/**
+ * Created by Gabriel on 25/05/2017.
+ */
+
+public class Usuario {
+
+    //Basic
+    public String email;
+    public String nome; //Nome Completo
+    public String endereço;
+    public Integer identidade;
+    public Integer cpf;
+    public Integer telefone;
+    public Integer celular;
+    public Integer cep;
+    public String tipoUsuario;
+
+    //Faculdade
+    public String matricula;
+    public String campus;
+    public String curso;
+
+    //IMGUR
+    public String urlImagem;
+    public String urlHash;
+}
